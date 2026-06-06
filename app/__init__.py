a738c19c4dc8eacd00c9baeaf8650cfff3c17d63
deffin05 <dm.melnyk05@gmail.com>
2026-06-06 14:13:20 +0300

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+
+from flask import Flask
+
+load_dotenv()
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY"),
+        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
