bbd17ba1c975cc516932ffc94ea474566f610ffc
deffin05 <dm.melnyk05@gmail.com>
2026-06-12 18:20:12 +0300

    Add constraints to the database schema

--- app/schema.sql
+++ app/schema.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS Employee;
-DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS Product;
-DROP TABLE IF EXISTS Store_Product;
-DROP TABLE IF EXISTS Customer_Card;
-DROP TABLE IF EXISTS "Check";
 DROP TABLE IF EXISTS Sale;
 DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS "Check";
+DROP TABLE IF EXISTS Store_Product;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Customer_Card;
 
 CREATE TABLE Employee (
     id_employee VARCHAR(10) PRIMARY KEY,
@@ -13,10 +13,16 @@ CREATE TABLE Employee (
     empl_name VARCHAR(50) NOT NULL,
     empl_patronymic VARCHAR(50),
     empl_role VARCHAR(10) NOT NULL,
-    salary DECIMAL(13, 4) NOT NULL,
+
+    salary DECIMAL(13, 4) NOT NULL CHECK ( salary >= 0 ),
+
     date_of_birth DATE NOT NULL,
     date_of_start DATE NOT NULL,
-    phone_number VARCHAR(13) NOT NULL,
+
+    phone_number VARCHAR(13) NOT NULL CHECK (
+        length(phone_number) <= 13 AND
+        phone_number LIKE '+%'
+        ),
     city VARCHAR(50) NOT NULL,
     street VARCHAR(50) NOT NULL,
     zip_code VARCHAR(9) NOT NULL
@@ -41,8 +47,8 @@ CREATE TABLE Store_Product (
     UPC VARCHAR(12) PRIMARY KEY,
     UPC_prom VARCHAR(12),
     id_product INTEGER NOT NULL,
-    selling_price DECIMAL(13,4) NOT NULL,
-    products_number INTEGER NOT NULL,
+    selling_price DECIMAL(13,4) NOT NULL CHECK ( selling_price >= 0 ),
+    products_number INTEGER NOT NULL CHECK ( products_number >= 0 ),
     promotional_product BOOLEAN NOT NULL,
     FOREIGN KEY (UPC_prom) REFERENCES Store_Product(UPC)
         ON UPDATE CASCADE
@@ -61,7 +67,7 @@ CREATE TABLE Customer_Card (
     city VARCHAR(50),
     street VARCHAR(50),
     zip_code VARCHAR(9),
-    percent INTEGER NOT NULL
+    percent INTEGER NOT NULL CHECK ( percent >= 0 )
 );
 
 CREATE TABLE "Check" (
@@ -69,8 +75,8 @@ CREATE TABLE "Check" (
     id_employee VARCHAR(10) NOT NULL,
     card_number VARCHAR(13),
     print_date DATETIME NOT NULL,
-    sum_total DECIMAL(13,4) NOT NULL,
-    vat DECIMAL(13,4) NOT NULL,
+    sum_total DECIMAL(13,4) NOT NULL CHECK ( sum_total >= 0 ),
+    vat DECIMAL(13,4) NOT NULL CHECK ( vat >= 0 ),
     FOREIGN KEY (id_employee) REFERENCES Employee(id_employee)
         ON UPDATE CASCADE
         ON DELETE NO ACTION,
@@ -82,8 +88,8 @@ CREATE TABLE "Check" (
 CREATE TABLE Sale (
     UPC VARCHAR(12) NOT NULL,
     check_number VARCHAR(10) NOT NULL,
-    product_number INTEGER NOT NULL,
-    selling_price DECIMAL(13,4) NOT NULL,
+    product_number INTEGER NOT NULL CHECK ( product_number > 0 ),
+    selling_price DECIMAL(13,4) NOT NULL CHECK ( selling_price >= 0 ),
     PRIMARY KEY (UPC, check_number),
     FOREIGN KEY (UPC) REFERENCES Store_Product(UPC)
         ON UPDATE CASCADE
