4025b0b1e61ca895d1553b5ed7cf17c191855a10
deffin05 <dm.melnyk05@gmail.com>
2026-06-12 14:59:01 +0300

    Move user retrieval to the User model

--- app/models.py
+++ app/models.py
@@ -9,10 +9,20 @@ class User(UserMixin):
         self.email = email
         self.password = password
 
-    def get(self, id_employee):
+    def get_by_id(self, id_employee):
         db = get_db()
         cursor = db.cursor()
-        cursor.execute("SELECT id_employee, email, password FROM users WHERE email = ?", (id_employee,))
+        cursor.execute("SELECT id_employee, email, password FROM users WHERE id_employee = ?", (id_employee,))
+
+        row = cursor.fetchone()
+        if row:
+            return User(row["id_employee"], row["email"], row["password"])
+        return None
+
+    def get_by_email(self, email):
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT id_employee, email, password FROM users WHERE email = ?", (email,))
 
         row = cursor.fetchone()
         if row:
