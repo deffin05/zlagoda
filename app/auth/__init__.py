0d0113ba4cb078bf33ed6836be39463ce63b453c
deffin05 <dm.melnyk05@gmail.com>
2026-06-13 21:49:28 +0300

    Move the decorator to a separate file

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,21 +1,6 @@
-from functools import wraps
 
-from flask import Blueprint, abort
-from flask_login import current_user
+from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__, url_prefix="/auth")
 
 from app.auth import routes
-
-def roles_required(*roles):
-    def decorator(f):
-        @wraps(f)
-        def wrapped(*args, **kwargs):
-            if not current_user.is_authenticated:
-                abort(401)
-            elif current_user.role not in roles:
-                abort(403)
-
-            return f(*args, **kwargs)
-        return wrapped
-    return decorator
