0b031bd6ac26ad4ef17fb0ee7c67f6b943b99c55
deffin05 <dm.melnyk05@gmail.com>
2026-06-14 15:50:03 +0300

    Add a logout functionality

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from werkzeug.security import check_password_hash
 
 from app.models import User
@@ -25,3 +25,11 @@ def login():
             flash('Invalid login credentials.', 'danger')
 
     return render_template("auth.html", form=form)
+
+
+@auth_bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "success")
+    return redirect(url_for("auth.login"))
