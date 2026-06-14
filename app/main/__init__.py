8388dd39b1c37c7962327547e59106c4e7c03f18
deffin05 <dm.melnyk05@gmail.com>
2026-06-14 13:24:39 +0300

    Add basic profile page

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 main_bp = Blueprint('main', __name__)
 
-from app.main import category, product, store_product, employee, customer, check
+from app.main import category, product, store_product, employee, customer, check, profile
