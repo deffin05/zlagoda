3dd12cb179886f2998637175c077c03a7f99a902
deffin05 <dm.melnyk05@gmail.com>
2026-06-15 10:27:11 +0300

    Add main stats page

---
+++ app/main/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from flask_login import login_required
+
+from app.main import main_bp
+
+
+@main_bp.route("/stats")
+@login_required
+def stats():
+    return render_template("stats/main.html")
+
+
+@main_bp.route("/stats/cashiers")
+@login_required
+def cashier_stats():
+    pass
+
+
+@main_bp.route("/stats/products")
+@login_required
+def product_stats():
+    pass
