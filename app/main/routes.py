53bfc7136cdb8fb736c4ef48ee36ef42ac31cd66
deffin05 <dm.melnyk05@gmail.com>
2026-06-15 15:46:17 +0300

    Finish the page for cashier stats

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 from flask import render_template, request
 from flask_login import login_required
 
@@ -22,12 +24,62 @@ def cashier_stats():
     db = get_db()
     cursor = db.cursor()
 
-    query = """SELECT id_employee, SUM(sum_total) cahier_sum FROM "Check" GROUP BY id_employee"""
+    cashiers = cursor.execute("""SELECT id_employee, empl_surname, empl_name
+                                 FROM Employee
+                                 WHERE empl_role = 'Касир'
+                                 ORDER BY empl_surname""").fetchall()
 
-    rows = cursor.execute(query).fetchall()
+    sub_query = """SELECT id_employee, SUM(sum_total) cashier_sum
+               FROM "Check" """
+    filters = []
+    params = []
 
-    return render_template("stats/cashiers.html", rows=rows, id_employee=id_employee, period=period,
-                           start_date=start_date, end_date=end_date)
+    if id_employee:
+        filters.append("id_employee = ?")
+        params.append(id_employee)
+
+    if period in ["today", "yesterday", "7", "30", "year", "custom"]:
+        filters.append("DATE(print_date) BETWEEN ? AND ?")
+        today = date.today()
+        match period:
+            case "today":
+                start_date = today
+                end_date = today
+            case "yesterday":
+                yesterday = today - timedelta(days=1)
+                start_date = yesterday
+                end_date = yesterday
+            case "7":
+                start_date = today - timedelta(days=6)
+                end_date = today
+            case "30":
+                start_date = today - timedelta(days=29)
+                end_date = today
+            case "year":
+                start_date = today - timedelta(days=365)
+                end_date = today
+        params.append(start_date)
+        params.append(end_date)
+    else:
+        period="all"
+
+    if filters:
+        sub_query += " WHERE " + " AND ".join(filters)
+
+    sub_query += " GROUP BY id_employee"
+
+    main_query = f"""SELECT E.id_employee, empl_surname, empl_name, empl_patronymic, cashier_sum
+                    FROM Employee E
+                    JOIN ({sub_query}) C ON E.id_employee = C.id_employee"""
+
+    rows = cursor.execute(main_query, params).fetchall()
+
+    total_sum = 0
+    for row in rows:
+        total_sum += row["cashier_sum"]
+
+    return render_template("stats/cashiers.html", rows=rows, selected_cashier_id=id_employee, period=period,
+                           start_date=start_date, end_date=end_date, today=date.today(), cashiers=cashiers, sum=total_sum)
 
 
 @main_bp.route("/stats/products")
