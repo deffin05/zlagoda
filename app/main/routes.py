edf403411b5cddfed56ec8d1fbcc126570d26af4
deffin05 <dm.melnyk05@gmail.com>
2026-06-15 12:23:59 +0300

    Add partially complete page of cashier sales stats

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import login_required
 
+from app.db import get_db
 from app.main import main_bp
 
 
@@ -13,7 +14,20 @@ def stats():
 @main_bp.route("/stats/cashiers")
 @login_required
 def cashier_stats():
-    return render_template("stats/cashiers.html")
+    id_employee = request.args.get("id_employee")
+    period = request.args.get("period")
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+
+    db = get_db()
+    cursor = db.cursor()
+
+    query = """SELECT id_employee, SUM(sum_total) cahier_sum FROM "Check" GROUP BY id_employee"""
+
+    rows = cursor.execute(query).fetchall()
+
+    return render_template("stats/cashiers.html", rows=rows, id_employee=id_employee, period=period,
+                           start_date=start_date, end_date=end_date)
 
 
 @main_bp.route("/stats/products")
